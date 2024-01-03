e1773455bfe0ef295a5ae599ccbd539018ddc0fd
Marc Sanchis <msanlli@teleco.upv.es>
2024-01-03 16:42:59 +0100

    v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,14 @@
 The first number symbolises the major version, the second the release, the third the patch.
 
 - v0
-  - v0.0.0
-    - Add initial story
-    - Add save handling
-  - v0.1.0
-    - Add opening options
-    - Add keydown detection
+  - v0.0
+    - v0.0.0
+      - Add initial story
+      - Add save handling
+  - v0.1
+    - v0.1.0
+      - Add opening options
+      - Add keydown detection
+    - v0.1.1
+      - Add manifesto
+      - Split code in functions
--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,104 @@ use std::{io, time::Duration, thread};
 use device_query::{DeviceQuery, DeviceState, Keycode};
 use clearscreen;
 
+const STDIN: io::Stdin = io::stdin();
+const INPUT: &mut String = &mut String::new();
+const DEVICE_STATE: DeviceState = DeviceState::new();
+
+fn clear() { clearscreen::clear().expect("ERR_cleaning_screen"); }
+
+struct Save {
+    client: String,
+    fleet: String,
+}
+
 fn main() {
-    let stdin = io::stdin();
-    let input = &mut String::new();
-    let option: u8;
-    let device_state = DeviceState::new();
+    let mut option: u8;
 
-    clearscreen::clear().expect("ERR_cleaning_screen");
-
-    println!("NomadSim v0.1.0\n@msanlli - 2023\n\n--------------------\n\nWelcome to NomadSim!\n\nChoose an option:\n \
-    1. New Game\n2. Load Game\n");
+    println!("NomadSim v0.2.0\n@msanlli - 2023\n--------------------\n\nWelcome to NomadSim!\n\n1. New Game\n\
+    2. Load Game\n\n>>> ");
 
     loop {
-        input.clear();
-        stdin.read_line(input).expect("ERR_reading_file");
+        let input = STDIN.read_line(INPUT).expect("ERR_reading_line");
         match input.trim().parse() {
-            Ok(num) => {option = num; break},
-            Err(_) => continue,
+            Ok(num) => {
+                option = num;
+                if option == 1 {
+                    new_game()
+                } else if option == 2 {
+
+                } else {
+                    println!("ERR_option_not_found");
+                    continue
+                };
+            },
+            Err(_) => {println!("ERR_reading_line"); continue;},
         };
-    };
-
-    if option == 1 {
-        println!("You accomplished your dreams and worked as a pilot for a major airline, but after\na couple years \
-        you realised that the commercial aviation wasn't for you, you\nneeded an adventure.\n\nYou decided to quit \
-        your job and start a company from scratch, you started to\npull some strings and one friend of yours told you \
-        about a logistic shortage in\nIceland because of the difficult terrain and the lack of roads,so you decided \
-        to\nstart a company that would help the locals transport goods quick and safely.\n\n Press any key to \
-        continue...\n");
-
-        thread::sleep(Duration::from_secs(1));
-
-        loop {
-            let keys: Vec<Keycode> = device_state.get_keys();
-            if !keys.is_empty() {
-                menu();
-                break;
-            };
-        };
-
-        } else if option == 2 {
-            println!("ERR_not_implemented");
-        } else {
-        println!("ERR_option_not_found");
     };
 }
 
-fn menu() {
-    println!("ERR_not_implemented")
+fn new_game() {
+    println!("\nYou accomplished your dreams and worked as a pilot for a major airline, but after\na couple years \
+        you realised that the commercial aviation wasn't for you, you\nneeded an adventure.\n\nYou decided to quit \
+        your job and start a company from scratch, you started to\npull some strings and one friend of yours told you \
+        about a logistic shortage in\nIceland because of the difficult terrain and the lack of roads,so you decided \
+        to\nstart a company that would help the locals transport goods quick and safely.\n\nPress any key to \
+        continue...\n");
+
+    thread::sleep(Duration::from_secs(1));
+
+    loop {
+        let keys: Vec<Keycode> = DEVICE_STATE.get_keys();
+        if !keys.is_empty() { new_save(); break; };
+    };
+
+   /*
+   for client_file, fleet_file = new_save(){
+        let mut save = Save {
+            client: client_file,
+            fleet: fleet_file,
+        };
+    }
+    */
+}
+
+fn new_save() { // -> (csv file, csv file)
+    print!("*Phone ringing*\n\n???- Hello? Hey, I'm Noah, the owner of the airstrip's hangar! You'll have to\npardon \
+    me but I forgot your name again... Can you remind it to me please?\n>>> ");
+
+    loop {
+        let input = STDIN.read_line(INPUT).expect("ERR_reading_line");
+        match input.trim().parse() {
+            Ok(str) => {
+                let pilot_handle = str;
+                // TODO! Initialise client.csv
+                break
+            },
+            Err(_) => {
+                println!("ERR_reading_line");
+                continue
+            },
+        };
+    };
+
+    // TODO! Create client.csv file
+
+    println!("Now it's time to select difficulty!:\n\n1. Easy\n    You start off at Keflavik International Airport \
+    (BIKF) which is the biggest\n    airport in Iceland. Your initial aircraft will be a Cessna 208B and your\n    \
+    initial debt is none, you'll even have 100.000€ in your account!\n"); // TODO! Create more modes
+
+    loop {
+        let input = STDIN.read_line(INPUT).expect("ERR_reading_line");
+        match input.trim().parse() {
+            Ok(num) => {
+                let option: u8 = num;
+                // TODO! Configure client and fleet
+                break
+            },
+            Err(_) => {
+                println!("ERR_reading_line");
+                continue
+            },
+        };
+    };
 }
